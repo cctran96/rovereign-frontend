00a648cfe33f0150d7f835aebbfbc26117b1f7f3
Chau Tran <cctran96@gmail.com>
2021-06-29 23:06:37 -0700

    more css changes

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -3,8 +3,28 @@ import { motion } from "framer-motion"
 import { AiOutlineUser, AiOutlineLock } from "react-icons/ai/index.esm"
 
 const Login = () => {
+    // Displays either login form or signup form
     const [showLogin, setShowLogin] = useState(true)
-    const toggleLogin = () => setShowLogin(!showLogin)
+    
+    // Controlled forms for both login and signup
+    const [loginForm, setLoginForm] = useState({username: "", password: ""})
+    const [signupForm, setSignUpForm] = useState({username: "", password: "", confirm: ""})
+    
+    const handleLoginChange = e => {
+        setLoginForm({...loginForm, [e.target.name]: e.target.value})
+    }
+
+    const handleSignupChange = e => {
+        setSignUpForm({...signupForm, [e.target.name]: e.target.value})
+    }
+
+    // Switches between login/signup and clears form
+    const toggleLogin = () => {
+        setShowLogin(!showLogin)
+        setLoginForm({username: "", password: ""})
+        setSignUpForm({username: "", password: "", confirm: ""})
+    }
+
 
     return (
         <div className="login-page">
@@ -17,17 +37,37 @@ const Login = () => {
                     <form>
                         <div className="field-container">
                             <AiOutlineUser size={30}/>
-                            <input name="username" placeholder="Username" required/>
+                            <input
+                                onChange={handleSignupChange}
+                                name="username" 
+                                placeholder="Username" 
+                                value={signupForm.username} 
+                                required
+                            />
                         </div>
                         <div className="field-container">
                             <AiOutlineLock size={30}/>
-                            <input name="password" placeholder="Password"type="password" required/>
+                            <input 
+                                onChange={handleSignupChange}
+                                name="password" 
+                                placeholder="Password"
+                                type="password" 
+                                value={signupForm.password} 
+                                required
+                            />
                         </div>
                         <div className="field-container">
                             <AiOutlineLock size={30}/>
-                            <input name="confirm" placeholder="Confirm Password"type="password" required/>
+                            <input 
+                                onChange={handleSignupChange}
+                                name="confirm" 
+                                placeholder="Confirm Password"
+                                type="password" 
+                                value={signupForm.confirm} 
+                                required
+                            />
                         </div>
-                        <button type="submit">Sign Up</button>
+                        <button type="submit">SIGN UP</button>
                     </form>
                 </motion.div>
                 <motion.div 
@@ -38,23 +78,36 @@ const Login = () => {
                     <form>
                         <div className="field-container">
                             <AiOutlineUser size={30}/>
-                            <input name="username" placeholder="Username" required/>
+                            <input
+                                onChange={handleLoginChange}
+                                name="username" 
+                                placeholder="Username" 
+                                value={loginForm.username} 
+                                required
+                            />
                         </div>
                         <div className="field-container">
                             <AiOutlineLock size={30}/>
-                            <input name="password" placeholder="Password"type="password" required/>
+                            <input
+                                onChange={handleLoginChange}
+                                name="password" 
+                                placeholder="Password"
+                                type="password" 
+                                value={loginForm.password} 
+                                required
+                            />
                         </div>
-                        <button type="submit">Sign In</button>
+                        <button type="submit">SIGN IN</button>
                     </form>
                 </motion.div>
                 <div className="overlay">
                     <motion.div className="left" variants={leftVar}>
                         <h2>Not a user?</h2>
-                        <button onClick={toggleLogin}>Sign up</button>
+                        <button onClick={toggleLogin}>SIGN UP</button>
                     </motion.div>
                     <motion.div className="right" variants={rightVar}>
                         <h2>Already a user?</h2>
-                        <button onClick={toggleLogin}>Sign in</button>
+                        <button onClick={toggleLogin}>SIGN IN</button>
                     </motion.div>
                 </div>
             </motion.div>
